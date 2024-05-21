b52e9665b817e1ad2b4ca8394791357f7b69ef1f
Sean Bee <esbee@outlook.com.au>
2024-05-21 11:53:36 +1000

    skibidi toilet

--- main.py
+++ main.py
@@ -24,3 +24,51 @@ while True:
             print('Incorrect Password')
         else:
             print('Your balance is: ', accountBalance)
+    elif action == 's':
+        print('Show: ')
+        print('     Name', accountName)
+        print('     Balance', accountBalance)
+        print('     Password', accountPassword)
+        print()
+
+    elif action == 'q':
+        break
+
+    elif action == 'w':
+        print('Withdraw: ')
+
+        userWithdrawAmount = input('Please enter your amount: ')
+
+        userWithdrawAmount = int(userWithdrawAmount)
+        userPassword = input('Please enter your password: ')
+
+        if userWithdrawAmount < 0:
+            print('You cannot withdraw this amount')
+
+        elif userPassword != accountPassword:
+            print('Incorrect password')
+        
+        elif userWithdrawAmount > accountBalance:
+            print('Not enough funds')
+
+        else:
+            accountBalance = accountBalance - userWithdrawAmount
+            print('Your new balance is: ' + str(accountBalance))
+
+    elif action == 'd':
+        print('Deposit: ')
+
+        userDepositAmount = input('Please enter your amount: ')
+
+        userDepositAmount = int(userDepositAmount)
+        userPassword = input('Please enter your password: ')
+
+        if userDepositAmount < 0:
+            print('You cannot deposit this amount')
+
+        elif userPassword != accountPassword:
+            print('Incorrect password')
+
+        else:
+            accountBalance = accountBalance + userDepositAmount
+            print('Your new balance is: ' + str(accountBalance))
